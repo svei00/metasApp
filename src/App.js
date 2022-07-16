d3b70fb7b83ef2088c18e17b8e29a121dc4e7367
svei00 <79055634+svei00@users.noreply.github.com>
2022-07-16 11:51:18 -0700

    added Goals component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Header from './components/share/js/Header'
 import Main from './components/share/js/Main'
 import Footer from './components/share/js/Footer'
+import Goal from './components/share/list/Goals'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
+      <Main>
+        <Goal></Goal>
+      </Main>
       <Footer></Footer>      
     </div>
   );
---
+++ src/components/share/list/Goals.js
@@ -0,0 +1,22 @@
+function Goal() {
+    return ( 
+        <div>
+            <div>
+                <div>Icon</div>
+                <p>1 <sub>/ Week</sub> </p>
+                <p>Details</p>
+            </div>
+            <div>
+                <div>
+                    <p>4 of 5</p>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+                <button>Complete!</button>
+            </div>
+        </div>
+     );
+}
+
+export default Goal;
